3ceae5bb87a23261e71336e49d2ada83e5a8199d
mohit-jarvis-sharma <mohit.jarvis.professional@gmail.com>
2017-03-14 22:57:14 +0530

    Added my project

---
+++ index.js
@@ -0,0 +1,26 @@
+var name = document.getElementById('name');
+var pass = document.getElementById('pass');
+pass.maxLength = "8";
+
+/*
+function validate()
+{
+	var x = document.forms[sign][username].value;
+	var y = document.forms[sign][password].value;
+
+
+	if(name.value == "admin" && pass.value == "admin")
+	{
+		location.href = "main.html";
+	}
+	else if(name.value == "user" && pass.value == "user")
+	{
+		location.href = "main.html";
+	}
+	else
+	{
+		alert("Wrong username or password.");
+	};
+}
+
+*/
